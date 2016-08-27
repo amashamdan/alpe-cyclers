b328e24ec1d8eedb544c8e11e1283f4ad4ed9d76
Amer Hamdan <amashamdan@gmail.com>
2016-08-26 17:40:36 -0700

    Feat: Axes created.
    The x and y axes are created and plotted with no data.
    x axis at this point is in seconds, it will be changed to mminutes later.

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ var url = "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/m
 /* AJAX request to fetch the cyclers data. */
 $.ajax ({
 	url: url,
-	data: "json",
+	dataType: "json",
 	success: function(data) {
 		/* The plot function which will build the chart is called. */
 		plot(data);
@@ -15,8 +15,44 @@ $.ajax ({
 });
 
 function plot(data) {
+	/* Chart is created and given dimension and white transparent background. */
 	var chart = d3.select("#chart-area").append("svg");
+	var height = 600;
+	var width = 900;
+	var padding = 50;
 	chart.attr("width", 900)
 		.attr("height", 600)
-		.style("background-color", "rgba(255, 255, 255, 0.5)");	
+		.style("background-color", "rgba(255, 255, 255, 0.5)");
+	for (var cycler in data) {
+		data[cycler].timeFromTop = data[cycler]["Seconds"] - 2210;
+	}
+	console.log(data);
+
+	var xScale = d3.scale.linear()
+					.domain([0, d3.max(data, function(d) {
+						return d['timeFromTop'];
+					})])
+					.range([width - padding, padding]);
+	var yScale = d3.scale.linear()
+					.domain([1, d3.max(data, function(d) {
+						return d['Place'];
+					})])
+					.range([padding, height - padding]);
+
+	var xAxis = d3.svg.axis()
+					.scale(xScale)
+					.orient("bottom");
+	chart.append("g")
+		.attr("class", "axis")
+		// brackets around height and padding are important or an error will occur
+		.attr("transform", "translate(0, "+ (height - padding) +")")
+		.call(xAxis);
+
+	var yAxis = d3.svg.axis()
+					.scale(yScale)
+					.orient("left");
+	chart.append("g")
+		.attr("class", "axis")
+		.attr("transform", "translate("+ padding +",0)")
+		.call(yAxis);
 }
